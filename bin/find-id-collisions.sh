66e9541c243b5aaff31d4904e4595ee213d5a625
Adrien Thebo <adrien@lagrange-automation.io>
2020-02-04 17:10:16 -0300

    Add script to find local/LDAP ID collisions

---
+++ bin/find-id-collisions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "user,olduid,oldgid,newuid,newgid"
+getent passwd | while read line; do
+  unset params
+  declare -a params
+  IFS=":" params=($line)
+  unset IFS
+  user="${params[0]}"
+  olduid="${params[2]}"
+  oldgid="${params[3]}"
+
+  if [[ $olduid -ge 1000 ]] && ipa user-find --login "$user" 2>/dev/null 1>/dev/null; then
+    newuid="$(ipa user-find --login $user | awk '/UID/ { print $2 }')"
+    newgid="$(ipa user-find --login $user | awk '/GID/ { print $2 }')"
+
+    echo "${params[0]},${olduid},${oldgid},${newuid},${newgid}"
+  fi
+done
